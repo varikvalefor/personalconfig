a9fd47233f5ce366fd467aff32fc2f28b6bbc721
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-02-09 19:25:29 -0500

    Create xmonad.hs

---
+++ xmonad.hs
@@ -0,0 +1,21 @@
+import XMonad;
+import XMonad.Util.Run;
+import XMonad.Layout.Grid;
+import XMonad.Hooks.ManageDocks;
+import XMonad.Hooks.UrgencyHook;
+import XMonad.Hooks.DynamicLog (xmobar);
+import XMonad.Layout.NoBorders (noBorders, smartBorders);
+
+myLayout =
+  smartBorders $
+  Grid ||| noBorders Full ||| Tall 1 (3/100) (1/2)
+
+conph = def {
+  modMask = mod4Mask
+  , terminal = "starch"
+  , layoutHook = myLayout
+  , normalBorderColor = "#008080"
+  , focusedBorderColor = "#00FFFF"
+}
+
+main = xmonad =<< xmobar conph
