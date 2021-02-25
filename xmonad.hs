1b1ce09d72dcae2ae8a3fa44130844fbc96b01fa
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-02-25 18:16:25 -0500

    XMOBAR is disabled in xmonad.hs.

--- xmonad.hs
+++ xmonad.hs
@@ -1,8 +1,9 @@
-import XMonad;
 import XMonad.Util.Run;
 import XMonad.Layout.Grid;
+import XMonad;
 import XMonad.Hooks.ManageDocks;
 import XMonad.Hooks.UrgencyHook;
+-- import XMonad.Layout.Spacing;
 import XMonad.Hooks.DynamicLog (xmobar);
 import XMonad.Layout.NoBorders (noBorders, smartBorders);
 
@@ -10,12 +11,14 @@ myLayout =
   smartBorders $
   Grid ||| noBorders Full ||| Tall 1 (3/100) (1/2)
 
+-- COLOUREDS
+
 conph = def {
-  modMask = mod4Mask
+  modMask = mod4Mask -- Use Super instead of Alt
   , terminal = "starch"
   , layoutHook = myLayout
   , normalBorderColor = "#008080"
   , focusedBorderColor = "#00FFFF"
 }
 
-main = xmonad =<< xmobar conph
+main = xmonad conph;
