fd110d02058815a3e1ab766e043b2cd6adc74b61
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-02-25 18:17:50 -0500

    xmonad.hs is rearranged slightly.  Some comments are removed from xmonad.hs.

--- xmonad.hs
+++ xmonad.hs
@@ -1,9 +1,8 @@
+import XMonad;
 import XMonad.Util.Run;
 import XMonad.Layout.Grid;
-import XMonad;
 import XMonad.Hooks.ManageDocks;
 import XMonad.Hooks.UrgencyHook;
--- import XMonad.Layout.Spacing;
 import XMonad.Hooks.DynamicLog (xmobar);
 import XMonad.Layout.NoBorders (noBorders, smartBorders);
 
@@ -11,10 +10,8 @@ myLayout =
   smartBorders $
   Grid ||| noBorders Full ||| Tall 1 (3/100) (1/2)
 
--- COLOUREDS
-
 conph = def {
-  modMask = mod4Mask -- Use Super instead of Alt
+  modMask = mod4Mask
   , terminal = "starch"
   , layoutHook = myLayout
   , normalBorderColor = "#008080"
